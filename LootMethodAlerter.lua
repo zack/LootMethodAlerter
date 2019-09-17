1d3767efc629a16b798873aa864b0023d5137150
Zack Youngren <zack@youngren.io>
2019-09-16 21:09:18 -0400

    v1.0

---
+++ LootMethodAlerter.lua
@@ -0,0 +1,58 @@
+--[[
+	Author: Zack Youngren
+	License: GNU AGPLv3
+]]
+
+------------------------
+-- Start User Options --
+------------------------
+
+-- Shows a raid warning that only the player will see
+local show_private_warning = true;
+
+-- Plays a sound that only the player will hear
+local play_private_sound = true;
+
+-- Messages the party on behalf of the player
+local alert_party = false;
+
+-- Disable this alerting when the user is in a raid
+local disable_when_in_raid = true;
+
+----------------------
+-- End User Options --
+----------------------
+
+local LootMethodAlerter_EventFrame = CreateFrame("FRAME", "FooAddonFrame");
+LootMethodAlerter_EventFrame:RegisterEvent("PARTY_LOOT_METHOD_CHANGED");
+
+local loot_method_strings = {
+  ["needbeforegreed"] = "Need Before Greed",
+  ["group"] = "Group Loot",
+  ["master"] = "Master Looter",
+  ["roundrobin"] = "Round Robin",
+  ["freeforall"] = "Free For All",
+};
+
+local function onPartyLootMethodChanged(self, event, ...)
+  if disable_when_in_raid and IsInRaid() then
+    return;
+  end
+
+  local lootmethod = GetLootMethod();
+  local warning_message = "Loot method has been set to " .. loot_method_strings[lootmethod];
+
+  if show_private_warning then
+	RaidNotice_AddMessage(RaidWarningFrame, "|cffff0000" .. "WARNING: " .. "|r" .. warning_message, ChatTypeInfo["RAID_WARNING"]);
+  end
+
+  if play_private_sound then
+    PlaySound(8959) -- RAID_WARNING;
+  end
+
+  if alert_party then
+    SendChatMessage("Loot Method Alerter: " .. warning_message, "PARTY");
+  end
+end
+
+LootMethodAlerter_EventFrame:SetScript("OnEvent", onPartyLootMethodChanged);
---
+++ README.md
@@ -0,0 +1,35 @@
+# LootMethodAlerter
+A small addon to loudly alert you when the loot method is changed.
+
+### Notes
+
+LootMethodAlerter will alert you when loot method has been changed. E.g. from Group Loot to Master Loooter.
+
+Alert methods and options can be changed by setting the values in LootMethodAlterter.lua.
+
+Alert methods:
+1. A raid warning (on by default)
+2. A raid warning sound (on by default)
+3. A message sent to the party (off by default)
+
+Options:
+
+1. Suspend addon while in a raid group (on by default; will suspend while in raid group)
+
+Known bugs:
+
+1. Alerts when the group type is changed from a raid to a party.
+
+
+### Installation
+
+- [Download](https://github.com/zack/LootMethodAlerter/archive/master.zip) the latest version of LootMethodAlerter directly from the repository and extract it into your `WoW/Interface/AddOns/` folder.
+- Rename `LootMethodAlerter-master` to `LootMethodAlerter`
+- Run World of Warcraft and make sure to enable this addon in the character select screen
+- You may change certain configuration settings inside LootMethodAlerter.lua
+
+
+License
+----
+
+GNU AGPLv3
